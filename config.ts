4e8b8756fad46f32b1b49d98e1cda7cc4a39ea36
pstlouis <patrick.st-louis@opsecid.ca>
2024-10-22 02:39:29 -0400

    branding
    Signed-off-by: pstlouis <patrick.st-louis@opsecid.ca>

--- config.ts
+++ config.ts
@@ -3,20 +3,8 @@ export const VERIFIER_PLUS_URL = env['VERIFIER_PLUS_URL'] || 'https://verifierpl
 
 export const KnownDidRegistries = [
   {
-    'name': 'DCC Pilot Registry',
-    'url': 'https://digitalcredentials.github.io/issuer-registry/registry.json'
-  },
-  {
-    'name': 'DCC Sandbox Registry',
-    'url': 'https://digitalcredentials.github.io/sandbox-registry/registry.json'
-  },
-  {
-    'name': 'DCC Community Registry',
-    'url': 'https://digitalcredentials.github.io/community-registry/registry.json'
-  },
-  {
-    'name': 'DCC Registry',
-    'url': 'https://digitalcredentials.github.io/dcc-registry/registry.json'
+    'name': 'OpSecId Dev Registry',
+    'url': 'https://registry.opsec.id/dev.json'
   }
 ];
 
